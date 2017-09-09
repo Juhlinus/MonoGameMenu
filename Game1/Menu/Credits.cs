967a454e1bb4782b249360c4e587446655f2a140
Linus Juhlin <juhlinus@gmail.com>
2017-09-09 16:22:21 +0200

    Reworked how Menu works

---
+++ Game1/Menu/Credits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Game1.Menu
+{
+	class Credits : BaseMenu
+	{
+		static readonly string[] Items = { "Who did it", "Back" };
+
+		Game _game;
+
+		public Credits(Game game) : base(game, Items)
+		{
+			_game = game;
+		}
+
+		public override void MenuAction()
+		{
+			Game.Components.Remove(this);
+
+			switch (Items[_selection])
+			{
+				case ("Who did it"):
+					break;
+				case ("Back"):
+					Game.Components.Add(new Menu.MainMenu(_game));
+					break;
+				default:
+					throw new ArgumentException("\"" + Items[_selection] + "\" is not a valid case");
+			}
+		}
+	}
+}
--- Game1/Menu/MainMenu.cs
+++ Game1/Menu/MainMenu.cs
@@ -1,131 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace Game1.Menu
 {
-	public class MainMenu : DrawableGameComponent
+	class MainMenu : BaseMenu
 	{
-		GraphicsDeviceManager _graphics;
+		static readonly string[] Items = { "New Game", "High Scores", "Options", "Credits", "Quit" };
 
-		SpriteBatch _spriteBatch;
-		SpriteFont _menuItem;
-		SpriteFont _title;
+		Game _game;
 
-		Texture2D[] _selectionBoxes;
-
-		KeyboardState _oldState;
-
-		bool gameStart_;
-		int _selection;
-		string[] _items;
-
-		public MainMenu(Game game /*, GameLoop gameLoop */) : base(game)
+		public MainMenu(Game game) : base(game, Items)
 		{
-			// gameStart_ = gameStart;
+			_game = game;
 		}
 
-		public override void Initialize()
-		{
-			_spriteBatch = (SpriteBatch) Game.Services.GetService(typeof(SpriteBatch));
-			_graphics = (GraphicsDeviceManager)Game.Services.GetService(typeof(GraphicsDeviceManager));
-
-			_menuItem = Game.Content.Load<SpriteFont>("MenuItem");
-			_title = Game.Content.Load<SpriteFont>("Title");
-
-			_selection = 0;
-
-			_items = new[] {"New Game", "High Scores", "Options", "Credits", "Quit"};
-
-			_selectionBoxes = new Texture2D[ _items.Length ];
-
-			for (int i = 0; i < _items.Length; i++)
-			{
-				_selectionBoxes[i] = new Texture2D(_graphics.GraphicsDevice, (int) _menuItem.MeasureString(_items[i]).X + 10,
-					(int) _menuItem.MeasureString(_items[i]).Y + 5);
-			}
-
-			foreach (var select in _selectionBoxes)
-			{
-				Color[] data = new Color[ select.Width * select.Height ];
-
-				for (int i = 0; i < data.Length; i++)
-				{
-					data[i] = Color.Chocolate;
-					select.SetData(data);
-				}
-			}
-
-			_oldState = Keyboard.GetState();
-
-			base.Initialize();
-		}
-
-		public override void Update(GameTime gameTime)
-		{
-			base.Update(gameTime);
-
-			KeyboardState newState = Keyboard.GetState();
-
-			var newPressedKeys = from k in newState.GetPressedKeys()
-				where !(_oldState.GetPressedKeys().Contains(k))
-				select k;
-
-			IEnumerable<Keys> pressedKeys = newPressedKeys as Keys[] ?? newPressedKeys.ToArray();
-
-			if (pressedKeys.Contains(Keys.Down))
-			{
-				_selection++;
-				_selection %= _items.Length;
-			}
-			else if (pressedKeys.Contains(Keys.Up))
-			{
-				_selection--;
-				_selection = _selection < 0 ? _items.Length - 1 : _selection;
-			}
-			else if (pressedKeys.Contains(Keys.Enter))
-			{
-				MenuAction();
-			}
-
-			_oldState = newState;
-		}
-
-		public override void Draw(GameTime gameTime)
-		{
-			base.Draw(gameTime);
-
-			_spriteBatch.Begin();
-
-			_spriteBatch.DrawString(_title, "Fuck you guys", new Vector2(_graphics.PreferredBackBufferWidth / 2 - 110, 75),
-				Color.White);
-
-			Vector2 itemPosition;
-			itemPosition.X = _graphics.PreferredBackBufferWidth / 2 - 100;
-
-			for (int i = 0; i < _items.Length; i++)
-			{
-				itemPosition.Y = _graphics.PreferredBackBufferHeight / 2 - 90 + 60 * i;
-
-				if (i == _selection)
-				{
-					_spriteBatch.Draw(_selectionBoxes[i], new Vector2(itemPosition.X - 4, itemPosition.Y - 2), Color.White);
-				}
-
-				_spriteBatch.DrawString(_menuItem, _items[i], itemPosition, Color.Yellow);
-			}
-
-			_spriteBatch.End();
-		}
-
-		void MenuAction()
+		public override void MenuAction()
 		{
 			Game.Components.Remove(this);
 
-			switch (_items[_selection])
+			switch (Items[_selection])
 			{
 				case ("New Game"):
 					break;
@@ -134,12 +27,13 @@ namespace Game1.Menu
 				case ("Options"):
 					break;
 				case ("Credits"):
+					Game.Components.Add(new Menu.Credits(_game));
 					break;
 				case ("Quit"):
 					Game.Exit();
 					break;
 				default:
-					throw new ArgumentException("\"" + _items[_selection] + "\" is not a valid case");
+					throw new ArgumentException("\"" + Items[_selection] + "\" is not a valid case");
 			}
 		}
 	}
